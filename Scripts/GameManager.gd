8b10013756462315ac67d73a6db0e5f5808c82d3
Kottalo <kottaloflotenspieler@gmail.com>
2024-08-08 02:50:00 +0800

    Checkpoint

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -5,6 +5,10 @@ var slotBoardSize: int = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	await get_tree().process_frame
+	
+	get_tree().get_root().size_changed.connect(_on_window_resized)
+	
 	var diceSlots = $"HBoxContainer/MidPanel/VBoxContainer/DiceSlots"
 	
 	var centerNode = ColorRect.new()
@@ -50,10 +54,39 @@ func _ready():
 				childNode.position = Vector2(childX, childY)
 				
 				diceSlots.add_child(childNode)
-
+				
+	var diceLobby = $"HBoxContainer/LeftPanel/VBoxContainer/Section4/HBoxContainer/DiceLobby"
+	
+	var gridSize = Vector2(8, 4)
+	var spacing = Vector2(10, 10)
+	
+	var dividedSizeX = ( diceLobby.size.x - (spacing.x * (gridSize.x - 1)) ) / gridSize.x
+	var dividedSizeY = ( diceLobby.size.y - (spacing.y * (gridSize.y - 1)) ) / gridSize.y
+	
+	var cellLength = min(dividedSizeX, dividedSizeY)
+	
+	for i in range(8):
+		for j in range(4):
+			var cell = ColorRect.new()
+			
+			cell.size = Vector2(cellLength, cellLength)
+			
+			diceLobby.add_child(cell)
+			
+			var cellX = i * cellLength + (i * spacing.x)
+			var cellY = j * cellLength + (j * spacing.y)
+			
+			cell.position = Vector2(cellX, cellY)
+			
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	pass
+
+# This function is called whenever the window is resized.
+func _on_window_resized():
+	pass
+	
